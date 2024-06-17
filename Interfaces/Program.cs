14050e4be127abad83756640bf645fbbc31db7c0
Shabel Gumah <sgumah001@st.ug.edu.gh>
2024-06-17 18:30:53 +0000

    Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Interfaces
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move() {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+}
