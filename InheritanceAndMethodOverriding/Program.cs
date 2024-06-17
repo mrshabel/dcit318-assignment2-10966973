270ac5134203a27b77610b7252650b3b87cd045a
Shabel Gumah <sgumah001@st.ug.edu.gh>
2024-06-17 17:40:43 +0000

    Inheritance and Method Overriding

--- InheritanceAndMethodOverriding/Program.cs
+++ InheritanceAndMethodOverriding/Program.cs
@@ -1,9 +1,45 @@
 ﻿namespace InheritanceAndMethodOverriding
 {
-    internal class Program
+    class Animal
+    {
+        // add virtual keyword to allow for method overriding
+        public virtual void MakeSound() {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            // animal sound
+            animal.MakeSound();
+
+            // dog sound
+            dog.MakeSound();
+
+            // cat sound
+            cat.MakeSound();
+
             Console.WriteLine("Hello, World!");
         }
     }
